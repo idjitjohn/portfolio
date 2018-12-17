b50673963f617778392f1abd8175284730455516
Marson <marson.laza@gmail.com>
2018-12-17 11:47:49 +0300

    Updates

---
+++ precache-manifest.0f941a6e464320e7e074c66397ecc011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf3842501ff98ecaafd2",
+    "url": "/static/js/main.bf384250.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "bf3842501ff98ecaafd2",
+    "url": "/static/css/main.59f921e1.chunk.css"
+  },
+  {
+    "revision": "c641f7ebfe9a0cfc7445089af436ecec",
+    "url": "/index.html"
+  }
+];
