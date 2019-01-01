05eb6320d99bad8cfbb4f06da65af32ecfc1a502
Marson <marson.laza@gmail.com>
2019-01-01 05:54:00 +0300

    Updates

---
+++ precache-manifest.fd7eaabbc5bc35d20661a0f77855d1f3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "081b11ebaca8ad30fd092e01451015dc",
+    "url": "/portfolio/static/media/Roboto-Regular.081b11eb.woff"
+  },
+  {
+    "revision": "f9fce7f4d8317880e71f",
+    "url": "/portfolio/static/css/main.dcdc9083.chunk.css"
+  },
+  {
+    "revision": "8d180b5661e62b17c08d",
+    "url": "/portfolio/static/js/1.8d180b56.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "303ded6436dcf7ea75157e2aeff876ce",
+    "url": "/portfolio/static/media/Roboto-Medium.303ded64.woff"
+  },
+  {
+    "revision": "90d3804f0231704c15ccc5861245e8ce",
+    "url": "/portfolio/static/media/Roboto-Thin.90d3804f.woff"
+  },
+  {
+    "revision": "37fbbbad5577a95bdf058307c717c882",
+    "url": "/portfolio/static/media/Roboto-Light.37fbbbad.woff"
+  },
+  {
+    "revision": "f9fce7f4d8317880e71f",
+    "url": "/portfolio/static/js/main.f9fce7f4.chunk.js"
+  },
+  {
+    "revision": "ad140ff02a7091257e2b31619106194e",
+    "url": "/portfolio/static/media/Roboto-Bold.ad140ff0.woff"
+  },
+  {
+    "revision": "8b1caad9e521832fc9307265fd85e53d",
+    "url": "/portfolio/static/media/moi.8b1caad9.jpg"
+  },
+  {
+    "revision": "5687afb091bef2ccd863d8d499138bcf",
+    "url": "/portfolio/static/media/bg2.5687afb0.jpg"
+  },
+  {
+    "revision": "bc37978fa679c0e6798ffc66bfcda3bf",
+    "url": "/portfolio/static/media/bg3.bc37978f.jpg"
+  },
+  {
+    "revision": "664ffd9352739b3ed3d79b8ea61e62c5",
+    "url": "/portfolio/static/media/bg4.664ffd93.jpg"
+  },
+  {
+    "revision": "f8ec5b09a971aee9250b64702b136e66",
+    "url": "/portfolio/static/media/bg5.f8ec5b09.jpg"
+  },
+  {
+    "revision": "1e7bef88921a22f26a7d9e723dbf860b",
+    "url": "/portfolio/index.html"
+  }
+];
